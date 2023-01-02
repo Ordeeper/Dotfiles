f7b531de07cb1e60232fff9103de9c0c22330347
Ordeeper <pedrordeeper@gmail.com>
2023-01-01 22:04:44 -0300

    refactor: Update polybar launch to verify polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+if pgrep -x polybar > /dev/null; then
+    # If all your bars have ipc enabled, you can use 
+    polybar-msg cmd quit
+    # Otherwise you can use the nuclear option:
+    # killall -q polybar
 
-# Launch skybar
-echo "---" | tee -a /tmp/polybar1.log
-polybar skybar 2>&1 | tee -a /tmp/polybar1.log & disown
+    sleep 0.03
+fi
+
+# Verify and Launch Skybar
+while ! pgrep -x polybar > /dev/null; do
+    echo "---" | tee -a /tmp/polybar1.log
+    polybar skybar 2>&1 | tee -a /tmp/polybar1.log & disown
+    sleep 0.3
+done
 
 echo "Bars launched..."
