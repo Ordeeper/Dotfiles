8a3bc80f20fd8aff84c3bf45156b932f517cd003
Ordeeper <pedrordeeper@gmail.com>
2023-03-07 07:55:29 -0300

    refactor: Improve comments, looping, and variables

--- .local/bin/draw_window.sh
+++ .local/bin/draw_window.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 # Dependencies slop, wmctrl, and xdotool
 
+# Takes the area geometry and position
 geometry=$(slop -f '0, %x, %y, %w, %h');
 
-# Checks if slop is executed successfully
-if [ $? -eq 0 ]; then
-        # Opens kitty and keeps the pid
-        /usr/bin/kitty &
-        id=$!;
+# Checks if slop was executed successfully
+if [ $? -eq 0 ]
+then
+    # Opens kitty and keeps the pid
+    /usr/bin/kitty &
+    pid=$!;
 
-        # Checks if kitty is opened
-        while ! [ $(xdotool search --pid $id) ]
-            do
-	       sleep 0.06;
-            done
+    # Checks if kitty is opened and takes the window id
+    until id=$(xdotool search --pid $pid)
+        do
+	    sleep 0.06;
+        done
 
-        # Toggles window to floating mode
-        id=$(xdotool search --pid $id);
-        i3-msg "[id=$id]" floating enable;
+    # Toggles window to floating mode
+    i3-msg "[id=$id]" floating enable;
 
-        # Takes the kitty id then reposition and resize
-        wmctrl -i -r "$id" -e "$geometry";
-	exit 0
-    else
-        exit 1
-    fi
+    # Takes the kitty id then reposition and resize
+    wmctrl -i -r "$id" -e "$geometry";
+    exit 0
+else
+    exit 1
+fi
