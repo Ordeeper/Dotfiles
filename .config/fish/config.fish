d02a683d484b3ef5681efb671f1a6ab0d8f537f2
Ordeeper <pedrordeeper@gmail.com>
2023-04-16 14:07:04 -0300

    feat: Add fish conf

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    # Import colorscheme from 'wal' asynchronously
+    cat ~/.cache/wal/sequences &
+
+    # Add components to $fish_user_paths
+    fish_add_path -g $HOME/.local/bin
+
+    # Hide the fish greeting
+    set -gx fish_greeting
+
+    # Enable vi mode
+    fish_vi_key_bindings
+
+    # Abbreviations
+    abbr -a tb taskbook
+end
