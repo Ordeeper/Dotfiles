a2c5ba710cc4c33710130325f2d61f3f6fb5d4c4
Ordeeper <pedrordeeper@gmail.com>
2023-05-03 13:22:06 -0300

    feat: Add dunst and launch conf and set up on i3

---
+++ .local/bin/launch_dunst.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Start dunst
+if pgrep -x dunst > /dev/null; then
+    killall -q dunst
+fi
+
+# Monitor the dunstrc file for changes
+while true; do
+  inotifywait -e modify $HOME/.config/dunst/dunstrc &> /dev/null
+  killall -q dunst
+  dunst &
+done
