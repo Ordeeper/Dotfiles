f7b9e69394a7bba5d490ef33821ffffb96db26e1
Ordeeper <pedrordeeper@gmail.com>
2023-08-14 15:00:03 -0300

    chore: Change expanding abbreviation keybind

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,9 @@ if status is-interactive
     # Hide the fish greeting
     set -g fish_greeting
 
+    # Default editor
+    set -gx EDITOR nvim
+
     # Enable vi mode
     fish_vi_key_bindings
     set fish_cursor_default block
@@ -18,18 +21,14 @@ if status is-interactive
     set fish_cursor_visual block
     set -g fish_vi_force_cursor 1
 
-    # Keybinds
+    # Custom bindings
     bind yy fish_clipboard_copy
     bind p fish_clipboard_paste
-
-    # Default editor
-    set -gx EDITOR nvim
-
-    # Custom bindings
     bind -M insert \cF accept-autosuggestion
+    bind -M insert \cC self-insert expand-abbr
+    bind -M insert ' ' self-insert
 
     # Abbreviations
-    abbr -a tb taskbook
     abbr -a cava TERM=xterm-kitty cava
 
     # Startup
