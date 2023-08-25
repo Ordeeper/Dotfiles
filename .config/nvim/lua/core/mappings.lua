85732967f8a88911a19838e2d3eafa2ada755e3b
Ordeeper <pedrordeeper@gmail.com>
2023-08-25 08:45:45 -0300

    chore: Update cmp keybinds

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -62,6 +62,21 @@ function M.plugins()
         vim.keymap.set("n", "<leader>h", ":NvimTreeToggle <CR>", {silent = true}) -- Toggle NvimTree
     end
 
+    --------- Cmp ---------
+    function plugins.cmp()
+        local cmp = require("cmp")
+        cmp.setup ({
+            mapping = cmp.mapping.preset.insert({
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+                ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<Tab>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            })
+        })
+    end
+
     --------- Lspconfig ---------
     function plugins.lspconfig()
         vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
@@ -99,7 +114,6 @@ function M.plugins()
             end
         })
     end
-
     return plugins
 end
 
--- .config/nvim/lua/plugins/configs/cmp.lua
+++ .config/nvim/lua/plugins/configs/cmp.lua
@@ -1,17 +1,12 @@
 local cmp = require "cmp"
+local mappings = require "core.mappings"
 
 cmp.setup ({
     window = {
         cmp.config.window.bordered(),
         Documentation = cmp.config.window.bordered(),
     },
-    mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
+    mappings.plugins.cmp(),
     sources = cmp.config.sources(
         {
             { name = "nvim_lsp" },
