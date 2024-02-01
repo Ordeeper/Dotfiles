3babcfddbc3411c03fc9f88fde7351a3c7ea4044
Ordeeper <pedrordeeper@gmail.com>
2024-02-01 16:52:07 -0300

    refactor(neovim): Add export module to Neorg

--- .config/nvim/lua/plugins/configs/neorg.lua
+++ .config/nvim/lua/plugins/configs/neorg.lua
@@ -1,13 +1,14 @@
 return {
     load = {
-        ["core.defaults"] = {},    -- Loads default behaviour
-        ["core.concealer"] = {},   -- Adds pretty icons to your documents
-        ["core.dirman"] = {        -- Manages Neorg workspaces
+        ["core.defaults"] = {},  -- Loads default behaviour
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.dirman"] = {      -- Manages Neorg workspaces
             config = {
                 workspaces = {
                     notes = "~/Notes",
                 },
             },
         },
+        ["core.export"] = {},
     }
 }
