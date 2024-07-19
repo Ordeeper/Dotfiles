e36da2c4b914ece3b46e886bfdc2e2abcf77dd2b
Ordeeper <pedrordeeper@gmail.com>
2024-07-19 18:05:38 -0300

    feat(polybar): Add support for multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-if pgrep -x polybar > /dev/null; then
-    # If all your bars have ipc enabled, you can use 
-    polybar-msg cmd quit
-    # Otherwise you can use the nuclear option:
-    # killall -q polybar
-
-    sleep 0.03
-fi
+killall -q polybar
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 # Launch Skybar
-echo "---" | tee -a /tmp/polybar1.log
-polybar skybar 2>&1 | tee -a /tmp/polybar1.log & disown
-
-echo "Bars launched..."
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload skybar &
+done
