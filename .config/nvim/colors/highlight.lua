f0850a30097c31d7a2500fa267d4ee977d1047a9
Ordeeper <pedrordeeper@gmail.com>
2024-08-19 02:00:33 -0300

    feat(neovim): Highlight the current line number

--- .config/nvim/colors/highlight.lua
+++ .config/nvim/colors/highlight.lua
@@ -1,12 +1,13 @@
---[[local opt = vim.opt
+local opt = vim.opt
 local api = vim.api
 
 local success, pywal_core = pcall(require, "pywal.core")
 
 if success then
     local colors = pywal_core.get_colors()
-    api.nvim_set_hl(0, "MatchParen", { bg = "#4A5057", fg = colors.color5 })
-    api.nvim_set_hl(0, "CursorLine", { bg = "#0E1117" })
+    --api.nvim_set_hl(0, "MatchParen", { bg = "#4A5057", fg = colors.color5 })
+    --api.nvim_set_hl(0, "CursorLine", { bg = "#0E1117" })
+    api.nvim_set_hl(0, "CursorLineNr", { fg = colors.color5 })
 end
 
 ------ Options ------
@@ -14,4 +15,3 @@ opt.cursorline = true
 
 ------ Colors ------
 api.nvim_set_hl(0, "StatusLine", { bold = true })
-]]
