2d47496b0a169469eff6560c7e86e9b7d8bd2b2a
Ordeeper <pedrordeeper@gmail.com>
2024-11-05 02:24:33 -0300

    feat(neovim): Improve Lualine layout

--- .config/nvim/lua/plugins/configs/lualine.lua
+++ .config/nvim/lua/plugins/configs/lualine.lua
@@ -15,42 +15,42 @@ custom_theme.command.c.bg = "None"
 custom_theme.inactive.c.bg = "None"
 
 return {
-    options = {
-        icons_enabled = true,
-        theme = custom_theme,
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = true,
-        refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-        },
-    },
-    sections = {
-        lualine_a = { "mode", "vim.fn.fnamemodify(vim.fn.getcwd(), ':t')" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = { { "filetype", colored = false }, "progress" },
-        lualine_z = { "location" }
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = { "lazy" }
+	options = {
+		icons_enabled = true,
+		theme = custom_theme,
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = {
+			statusline = {},
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		globalstatus = true,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		},
+	},
+	sections = {
+		lualine_a = { "mode", "vim.fn.fnamemodify(vim.fn.getcwd(), ':t')" },
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = { "diagnostics", "diff", { "filetype", colored = false } },
+		lualine_z = { { "branch", icons_enabled = false }, "progress" }
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {}
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = { "lazy" }
 }
