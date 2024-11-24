6b2deab405da00109351ba84fb590030727b53f0
Ordeeper <pedrordeeper@gmail.com>
2024-11-23 21:01:50 -0300

    refactor(telescope): Improve UI

--- .config/nvim/lua/plugins/configs/telescope_conf.lua
+++ .config/nvim/lua/plugins/configs/telescope_conf.lua
@@ -16,8 +16,16 @@ return {
 		sorting_strategy = "ascending",
 		file_ignore_patterns = { "^.git/" },
 		prompt_prefix = "   ",
-		selection_caret = "  ",
-		entry_prefix = "  ",
+		selection_caret = " ",
+		entry_prefix = " ",
+		layout_config = {
+			horizontal = {
+				prompt_position = "top",
+				preview_width = 0.55,
+			},
+			width = 0.87,
+			height = 0.80,
+		},
 	},
 	pickers = {
 		find_files = {
@@ -29,17 +37,18 @@ return {
 	},
 	extensions = {
 		fzf = {
-			fuzzy = true,                   -- False will only do exact matching
+			fuzzy = true,          -- False will only do exact matching
 			override_generic_sorter = true, -- Override the generic sorter
-			override_file_sorter = true,    -- Override the file sorter
-			case_mode = "smart_case",       -- Or "ignore_case" or "respect_case"
+			override_file_sorter = true, -- Override the file sorter
+			case_mode = "smart_case", -- Or "ignore_case" or "respect_case"
 		},
 		file_browser = {
 			-- Disables netrw and use telescope-file-browser in its place
-			hijack_netrw = true,
-			mappings = mapping_file_browser,
-			depth = 5,
+			hijack_netrw      = true,
+			mappings          = mapping_file_browser,
+			depth             = 5,
 			respect_gitignore = true,
+			display_stat      = false,
 		},
 	}
 }
