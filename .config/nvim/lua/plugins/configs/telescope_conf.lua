33326a0dd0cb3494d7b32510c74549c207913203
Ordeeper <pedrordeeper@gmail.com>
2025-01-04 18:20:53 -0300

    refactor(telescope): Change icon

--- .config/nvim/lua/plugins/configs/telescope_conf.lua
+++ .config/nvim/lua/plugins/configs/telescope_conf.lua
@@ -15,7 +15,7 @@ return {
 		mappings = mapping_telescope,
 		sorting_strategy = "ascending",
 		file_ignore_patterns = { "^.git/" },
-		prompt_prefix = "   ",
+		prompt_prefix = "   ",
 		selection_caret = " ",
 		entry_prefix = " ",
 		layout_config = {
@@ -33,22 +33,22 @@ return {
 		},
 		buffers = {
 			mappings = mapping_buffers,
-		}
+		},
 	},
 	extensions = {
 		fzf = {
-			fuzzy = true,          -- False will only do exact matching
+			fuzzy = true, -- False will only do exact matching
 			override_generic_sorter = true, -- Override the generic sorter
 			override_file_sorter = true, -- Override the file sorter
 			case_mode = "smart_case", -- Or "ignore_case" or "respect_case"
 		},
 		file_browser = {
 			-- Disables netrw and use telescope-file-browser in its place
-			hijack_netrw      = true,
-			mappings          = mapping_file_browser,
-			depth             = 5,
+			hijack_netrw = true,
+			mappings = mapping_file_browser,
+			depth = 5,
 			respect_gitignore = true,
-			display_stat      = false,
+			display_stat = false,
 		},
-	}
+	},
 }
