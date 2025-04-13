20710b4b923af0902863b07b40c6614e8f42d92b
Ordeeper <pedrordeeper@gmail.com>
2025-04-12 21:49:20 -0300

    feat(nvim) Split mappings for Neovim and Telescope

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -11,7 +11,7 @@ function M.setup()
 	M.normal()
 	M.visual()
 	M.terminal()
-	M.plugins = M.plugins() -- Get the returned object
+	M.plugins = M.plugins()
 end
 
 function M.global()
@@ -19,41 +19,41 @@ function M.global()
 end
 
 function M.insert()
-	--------- Go to Beginning and End ---------
-	keymap("i", "<C-b>", "<ESC>^i") -- Beginning of Line
-	keymap("i", "<C-e>", "<END>") -- End of Line
+	--------- Miscellaneous ---------
 
 	--------- Navigate Within Insert Mode ---------
-	keymap("i", "<C-h>", "<Left>") -- Move Left
-	keymap("i", "<C-l>", "<Right>") -- Move Right
-	keymap("i", "<C-k>", "<Up>") -- Move Up
-	keymap("i", "<C-j>", "<Down>") -- Move Down
+	keymap("i", "<C-h>", "<Left>", { noremap = true }) -- Move Left
+	keymap("i", "<C-l>", "<Right>", { noremap = true }) -- Move Right
+	keymap("i", "<C-k>", "<Up>", { noremap = true }) -- Move Up
+	keymap("i", "<C-j>", "<Down>", { noremap = true }) -- Move Down
 end
 
 function M.normal()
 	keymap("n", "<ESC>", "<cmd> noh <CR>", { silent = true }) -- Clear Highlights
 
 	--------- Miscellaneous ---------
-	keymap("n", "<C-s>", "<cmd> w <CR>")          -- Save File
-	keymap("n", "<C-x>", "<cmd> x <CR>")          -- Save and Exit
-	keymap("n", "<C-q>", "<cmd> q! <CR>")         -- Don't Save and Exit
-	keymap("n", "<leader>n", "<cmd> set nu! <CR>") -- Toggle Line Number
-	keymap("n", "<leader>rn", "<cmd> set rnu! <CR>") -- Toggle Relative Number
-	keymap("n", "<leader>rn", "<cmd> set rnu! <CR>") -- Toggle Relative Number
-
+	keymap("n", "<C-s>", "<cmd> w <CR>", { noremap = true, silent = true }) -- Save File
+	keymap("n", "<C-x>", "<cmd> x <CR>", { noremap = true, silent = true }) -- Save and Exit
+	keymap("n", "<C-q>", "<cmd> q! <CR>", { noremap = true, silent = true }) -- Don't Save and Exit
+	keymap("n", "<leader>n", "<cmd> set nu! <CR>", { noremap = true, silent = true }) -- Toggle Line Number
+	keymap("n", "<leader>rn", "<cmd> set rnu! <CR>", { noremap = true, silent = true }) -- Toggle Relative Number
+	keymap("n", "<leader>rn", "<cmd> set rnu! <CR>", { noremap = true, silent = true }) -- Toggle Relative Number
+	keymap("n", "<C-p>", "<C-b>", { noremap = true, silent = true })
 
 	--------- Switch Between Windows ---------
-	keymap("n", "<C-h>", "<C-w>h") -- Window Left
-	keymap("n", "<C-l>", "<C-w>l") -- Window Right
-	keymap("n", "<C-k>", "<C-w>k") -- Window Up
-	keymap("n", "<C-j>", "<C-w>j") -- Window Down
+	keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true }) -- Window Left
+	keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true }) -- Window Right
+	keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true }) -- Window Up
+	keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true }) -- Window Down
 
 	--------- Buffers  ---------
-	keymap("n", "<tab>", "<cmd> bn <CR>")     -- Next Buffer
-	keymap("n", "<S-tab>", "<cmd> bp <CR>")   -- Preview Buffer
-	keymap("n", "<leader>ch", "<cmd> enew <CR>") -- New Buffer
-	keymap("n", "<C-d>", "<cmd> bd! <CR>")    -- Kill Buffer
-	keymap("n", "<C-c>", "<cmd> w | bd <CR>") -- Save and Kill Buffer
+	keymap("n", "<tab>", "<cmd> bn <CR>", { noremap = true, silent = true }) -- Next Buffer
+	keymap("n", "<S-tab>", "<cmd> bp <CR>", { noremap = true, silent = true }) -- Preview Buffer
+	keymap("n", "<leader>ch", "<cmd> enew <CR>", { noremap = true, silent = true }) -- New Buffer
+	keymap("n", "<C-d>", "<cmd> bd! <CR>", { noremap = true, silent = true }) -- Kill Buffer
+	keymap("n", "<C-c>", "<cmd> w | bd <CR>", { noremap = true, silent = true }) -- Save and Kill Buffer
+	keymap("n", "<C-b>", "<cmd> vsplit <CR>", { noremap = true, silent = true })
+	keymap("n", "<C-n>", "<cmd> split <CR>", { noremap = true, silent = true })
 end
 
 function M.visual()
@@ -61,7 +61,7 @@ function M.visual()
 end
 
 function M.terminal()
-	keymap("t", "<C-d>", "<cmd> q! <CR>", { silent = true })
+	keymap("t", "<C-d>", "<cmd> q! <CR>", { noremap = true, silent = true })
 end
 
 function M.plugins()
@@ -69,9 +69,9 @@ function M.plugins()
 
 	--------- Telescope ---------
 	function plugins.telescope()
-		local actions = require "telescope.actions"
-		local fb_actions = require "telescope".extensions.file_browser.actions
-		local get_status = require "telescope.state".get_status
+		local actions = require("telescope.actions")
+		local fb_actions = require("telescope").extensions.file_browser.actions
+		local get_status = require("telescope.state").get_status
 		local select_one_or_multi = require("core.utils").select_one_or_multi
 		local focus_preview = require("core.utils").focus_preview
 
@@ -80,110 +80,115 @@ function M.plugins()
 		keymap("n", "<leader>j", "<cmd> Telescope buffers <CR>", { noremap = true })
 
 		return {
-				-- Mapping Telescope
-				i = {
-					["<CR>"] = actions.select_default,
-					["<C-t>"] = select_one_or_multi,
-					["<C-k>"] = actions.move_selection_previous,
-					["<C-j>"] = actions.move_selection_next,
-					["<C-u>"] = function(bufnr)
-						return focus_preview(bufnr, "<C-u>")
-					end,
-					["<C-b>"] = function(bufnr)
-						return actions.preview_scrolling_left(bufnr)
-					end,
-					["<C-g>"] = function(bufnr)
-						return actions.preview_scrolling_right(bufnr)
-					end,
-					["<C-v>"] = function(bufnr)
-						get_status(bufnr).picker.layout_config.scroll_speed = 1
-						return actions.preview_scrolling_up(bufnr)
-					end,
-					["<C-f>"] = function(bufnr)
-						get_status(bufnr).picker.layout_config.scroll_speed = 1
-						return actions.preview_scrolling_down(bufnr)
-					end,
-					["<ScrollWheelUp>"] = function(bufnr)
-						get_status(bufnr).picker.layout_config.scroll_speed = 1
-						return actions.preview_scrolling_up(bufnr)
-					end,
-					["<ScrollWheelDown>"] = function(bufnr)
-						get_status(bufnr).picker.layout_config.scroll_speed = 1
-						return actions.preview_scrolling_down(bufnr)
-					end,
-					["<C-d>"] = actions.close,
-				},
-				n = {
-					["<CR>"] = actions.select_default,
-					["<C-k>"] = actions.move_selection_previous,
-					["<C-j>"] = actions.move_selection_next,
-					["<C-t>"] = select_one_or_multi,
-					["<C-u>"] = function(bufnr)
-						return focus_preview(bufnr, "<C-u>")
-					end,
-					["<C-b>"] = function(bufnr)
-						return actions.preview_scrolling_left(bufnr)
-					end,
-					["<C-g>"] = function(bufnr)
-						return actions.preview_scrolling_right(bufnr)
-					end,
-					["<C-v>"] = function(bufnr)
-						get_status(bufnr).picker.layout_config.scroll_speed = 1
-						return actions.preview_scrolling_up(bufnr)
-					end,
-					["<C-f>"] = function(bufnr)
-						get_status(bufnr).picker.layout_config.scroll_speed = 1
-						return actions.preview_scrolling_down(bufnr)
-					end,
-					["<ScrollWheelUp>"] = function(bufnr)
-						get_status(bufnr).picker.layout_config.scroll_speed = 1
-						return actions.preview_scrolling_up(bufnr)
-					end,
-					["<ScrollWheelDown>"] = function(bufnr)
-						get_status(bufnr).picker.layout_config.scroll_speed = 1
-						return actions.preview_scrolling_down(bufnr)
-					end,
-					["<C-d>"] = actions.close,
-				}
+			-- Mapping Telescope
+			i = {
+				["<CR>"] = actions.select_default,
+				["<C-t>"] = select_one_or_multi,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-u>"] = function(bufnr)
+					return focus_preview(bufnr, "<C-u>")
+				end,
+				["<C-p>"] = function(bufnr)
+					return actions.preview_scrolling_left(bufnr)
+				end,
+				["<C-g>"] = function(bufnr)
+					return actions.preview_scrolling_right(bufnr)
+				end,
+				["<C-v>"] = function(bufnr)
+					get_status(bufnr).picker.layout_config.scroll_speed = 1
+					return actions.preview_scrolling_up(bufnr)
+				end,
+				["<C-f>"] = function(bufnr)
+					get_status(bufnr).picker.layout_config.scroll_speed = 1
+					return actions.preview_scrolling_down(bufnr)
+				end,
+				["<ScrollWheelUp>"] = function(bufnr)
+					get_status(bufnr).picker.layout_config.scroll_speed = 1
+					return actions.preview_scrolling_up(bufnr)
+				end,
+				["<ScrollWheelDown>"] = function(bufnr)
+					get_status(bufnr).picker.layout_config.scroll_speed = 1
+					return actions.preview_scrolling_down(bufnr)
+				end,
+				["<C-d>"] = actions.close,
+				["<C-b>"] = actions.select_vertical,
+				["<C-n>"] = actions.select_horizontal,
 			},
-			{
-				-- Mapping Telescope File Browser
-				i = {
-					["<C-c>"] = fb_actions.create,
-					["<C-x>"] = fb_actions.remove,
-					["<C-r>"] = fb_actions.rename,
-					["<C-p>"] = fb_actions.move,
-					["<C-y>"] = fb_actions.copy,
-					["<C-s>"] = fb_actions.toggle_all,
-					["<C-e>"] = fb_actions.goto_home_dir,
-					["<C-w>"] = fb_actions.goto_cwd,
-					["<C-q>"] = fb_actions.change_cwd,
-					["<C-a>"] = fb_actions.toggle_hidden,
-					["<C-o>"] = fb_actions.open,
-				},
-				n = {
-					["c"] = fb_actions.create,
-					["x"] = fb_actions.remove,
-					["r"] = fb_actions.rename,
-					["p"] = fb_actions.move,
-					["y"] = fb_actions.copy,
-					["s"] = fb_actions.toggle_all,
-					["e"] = fb_actions.goto_home_dir,
-					["w"] = fb_actions.goto_cwd,
-					["q"] = fb_actions.change_cwd,
-					["h"] = fb_actions.toggle_hidden,
-					["o"] = fb_actions.open,
-				}
+			n = {
+				["<CR>"] = actions.select_default,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-t>"] = select_one_or_multi,
+				["<C-u>"] = function(bufnr)
+					return focus_preview(bufnr, "<C-u>")
+				end,
+				["<C-p>"] = function(bufnr)
+					return actions.preview_scrolling_left(bufnr)
+				end,
+				["<C-g>"] = function(bufnr)
+					return actions.preview_scrolling_right(bufnr)
+				end,
+				["<C-v>"] = function(bufnr)
+					get_status(bufnr).picker.layout_config.scroll_speed = 1
+					return actions.preview_scrolling_up(bufnr)
+				end,
+				["<C-f>"] = function(bufnr)
+					get_status(bufnr).picker.layout_config.scroll_speed = 1
+					return actions.preview_scrolling_down(bufnr)
+				end,
+				["<ScrollWheelUp>"] = function(bufnr)
+					get_status(bufnr).picker.layout_config.scroll_speed = 1
+					return actions.preview_scrolling_up(bufnr)
+				end,
+				["<ScrollWheelDown>"] = function(bufnr)
+					get_status(bufnr).picker.layout_config.scroll_speed = 1
+					return actions.preview_scrolling_down(bufnr)
+				end,
+				["<C-d>"] = actions.close,
+				["<C-b>"] = actions.select_vertical,
+				["<C-n>"] = actions.select_horizontal,
+				["e"] = false,
 			},
-			{
-				-- Mapping Buffers
-				i = {
-					["<C-x>"] = actions.delete_buffer,
-				},
-				n = {
-					["x"] = actions.delete_buffer,
-				}
-			}
+		}, {
+			-- Mapping Telescope File Browser
+			i = {
+				["<C-c>"] = fb_actions.create,
+				["<C-x>"] = fb_actions.remove,
+				["<C-r>"] = fb_actions.rename,
+				["<C-m>"] = fb_actions.move,
+				["<C-y>"] = fb_actions.copy,
+				["<C-s>"] = fb_actions.toggle_all,
+				["<C-e>"] = fb_actions.goto_home_dir,
+				["<C-w>"] = fb_actions.goto_cwd,
+				["<C-q>"] = fb_actions.change_cwd,
+				["<C-a>"] = fb_actions.toggle_hidden,
+				["<C-o>"] = fb_actions.open,
+			},
+			n = {
+				["<C-c>"] = fb_actions.create,
+				["<C-x>"] = fb_actions.remove,
+				["<C-r>"] = fb_actions.rename,
+				["<C-m>"] = fb_actions.move,
+				["<C-y>"] = fb_actions.copy,
+				["<C-s>"] = fb_actions.toggle_all,
+				["<C-e>"] = fb_actions.goto_home_dir,
+				["<C-w>"] = fb_actions.goto_cwd,
+				["<C-q>"] = fb_actions.change_cwd,
+				["<C-h>"] = fb_actions.toggle_hidden,
+				["<C-o>"] = fb_actions.open,
+				["<C-b>"] = actions.select_vertical,
+				["<C-n>"] = actions.select_horizontal,
+			},
+		}, {
+			-- Mapping Buffers
+			i = {
+				["<C-x>"] = actions.delete_buffer,
+			},
+			n = {
+				["x"] = actions.delete_buffer,
+			},
+		}
 	end
 
 	--------- Zen Mode ---------
@@ -218,7 +223,7 @@ function M.plugins()
 		keymap("n", "<leader>R", "<cmd> NeovimProjectLoadRecent <CR>", { silent = true })
 		return {
 			i = "<C-x>",
-			n = "x"
+			n = "x",
 		}
 	end
 
@@ -232,12 +237,13 @@ function M.plugins()
 	function plugins.cmp()
 		local cmp = require("cmp")
 		return {
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			["<C-p>"] = cmp.mapping.scroll_docs(-4),
 			["<C-f>"] = cmp.mapping.scroll_docs(4),
 			["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
 			["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
 			["<C-e>"] = cmp.mapping.abort(),
 			["<Tab>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			["<C-n>"] = cmp.config.disable,
 		}
 	end
 
@@ -273,9 +279,9 @@ function M.plugins()
 				keymap({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 				keymap("n", "gr", vim.lsp.buf.references, opts)
 				keymap("n", "<leader>t", function()
-					vim.lsp.buf.format { async = true }
+					vim.lsp.buf.format({ async = true })
 				end, opts)
-			end
+			end,
 		})
 	end
 
