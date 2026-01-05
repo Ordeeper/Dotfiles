490fd71dc15bb3fd465df5d2786a980c26c820fe
Ordeeper <pedrordeeper@gmail.com>
2026-01-05 16:26:49 -0300

    fix: Greetd environment

--- modules/wayland/greetd.nix
+++ modules/wayland/greetd.nix
@@ -1,7 +1,25 @@
+{ pkgs, ... }:
+
 {
+  environment.systemPackages = with pkgs; [
+    tuigreet
+  ];
+
   services.greetd = {
     enable = true;
-    settings.default_session.command = "Hyprland";
+    settings = {
+      default_session = {
+        command = "tuigreet --cmd Hyprland";
+        user = "nix-user";
+      };
+    };
+  };
+
+  security.pam.services.greetd.enable = true;
+
+  programs.hyprland.enable = true;
+
+  environment.sessionVariables = {
+    XDG_SESSION_TYPE = "wayland";
   };
 }
-
