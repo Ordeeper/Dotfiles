747692437d960a14ab410f202b8ee2600dbbeeb3
Ordeeper <pedrordeeper@gmail.com>
2026-01-05 17:29:10 -0300

    refactor: Add git configuration

--- home/nix-user/home.nix
+++ home/nix-user/home.nix
@@ -5,12 +5,18 @@
   home.homeDirectory = "/home/nix-user";
   home.stateVersion = "24.11";
 
-  programs.git.enable = true;
   programs.neovim.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "Ordeeper";
+    userEmail = "pedrordeeper@gmail.com";
+    extraConfig = {
+      init = {defaultBranch = "main";};
+    };
+  };
 
   imports = [
     ./hyprland.nix
 	./apps/terminals/kitty.nix
   ];
 }
-
