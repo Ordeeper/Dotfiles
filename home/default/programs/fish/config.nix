48a281a96461d3f797d94d02be7107c82766c47b
Ordeeper <pedrordeeper@gmail.com>
2026-01-06 14:53:57 -0300

    refactor: Add tmux startup on fish

--- home/default/programs/fish/config.nix
+++ home/default/programs/fish/config.nix
@@ -14,12 +14,11 @@
       set fish_cursor_visual block
       set -g fish_vi_force_cursor 1
 
-      # Custom bindings
-      bind yy fish_clipboard_copy
-      bind p fish_clipboard_paste
-      bind -M insert \cF accept-autosuggestion
-      bind -M insert \cC self-insert expand-abbr
-      bind -M insert ' ' self-insert
+      if status is-interactive
+        and not set -q TMUX
+	and command -v tmux >/dev/null
+	  exec tmux
+      end
     '';
   };
   imports = [
