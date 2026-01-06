9107e7608c59cd5ac9b79a03b37d841ec14f33ee
Ordeeper <pedrordeeper@gmail.com>
2026-01-06 20:15:55 -0300

    feat: Add zed configuration

--- home/default/programs/zed/settings.nix
+++ home/default/programs/zed/settings.nix
@@ -1,3 +1,91 @@
 {
-  programs.zed-editor.enable = true;
+  pkgs,
+  lib,
+  ...
+}:
+{
+  programs.zed-editor = {
+    enable = true;
+    extensions = [
+      "nix"
+      "html"
+      "toml"
+      "dockerfile"
+      "php"
+      "sql"
+      "vue"
+      "tokyo-night"
+      "material-icon-theme"
+      "make"
+      "scss"
+      "dart"
+      "csv"
+      "ini"
+    ];
+    extraPackages = [ pkgs.nixd ];
+
+    userSettings = {
+      auto_install_extensions = true;
+      vim_mode = true;
+      show_edit_predictions = false;
+      telemetry = {
+	diagnostics = false;
+      	metrics = false;
+      };
+      ui_font_size = lib.mkForce 18;
+      buffer_font_size = lib.mkForce 17;
+      file_finder = {
+        modal_width = "medium";
+      };
+      autosave = {
+      	after_delay.milliseconds = 1000;
+      };
+      tabs = {
+        show_diagnostics = "errors";
+      };
+      git_panel = {
+      	dock = "right";
+      };
+      git = {
+      	inline_blame = {
+	  enabled = false;
+	};
+      };
+      indent_guides = {
+        enabled = true;
+        coloring = "indent_aware";
+      };
+      # centered_layout = {
+      #   left_padding = "0.15";
+      #   right_padding = "0.15";
+      # };
+      inlay_hints = {
+        enabled = true;
+      };
+      auto_update = false;
+      terminal = {
+        env = {
+          EDITOR = "zed --wait";
+          TERM = "kitty";
+        };
+        line_height = "comfortable";
+        button = true;
+      };
+      theme = {
+        mode = "dark";
+        light = "Tokyo Night";
+        dark = "Tokyo Night";
+      };
+      buffer_font_family = "Maple Mono NF";
+      icon_theme = "Material Icon Theme";
+      lsp = {
+        nix = {
+          binary = {
+            path_lookup = true;
+          };
+        };
+      };
+    };
+
+  };
 }
