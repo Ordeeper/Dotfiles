1959df20cea5fa742033d7e3a7107ec20f7d8afe
Ordeeper <pedrordeeper@gmail.com>
2026-01-09 12:02:55 -0300

    chore: Add font configuration imports

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -9,6 +9,7 @@
     ../../modules/system/networking.nix
     ../../modules/system/users.nix
     ../../modules/system/sound.nix
+    ../../modules/system/fonts.nix
     ../../modules/system/packages.nix
     ../../modules/system/nix.nix
     ../../modules/system/input/keyd.nix
--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -10,6 +10,7 @@
     ../../modules/system/networking.nix
     ../../modules/system/sound.nix
     ../../modules/system/users.nix
+    ../../modules/system/fonts.nix
     ../../modules/system/nix.nix
     ../../modules/system/input/keyd.nix
 
