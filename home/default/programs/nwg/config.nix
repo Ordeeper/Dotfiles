8039a5d123752e49df659332cb6459ce492742c4
Ordeeper <pedrordeeper@gmail.com>
2026-03-07 00:50:19 -0300

    feat: Add nwg-displays and notification

--- home/default/programs/nwg/config.nix
+++ home/default/programs/nwg/config.nix
@@ -1,16 +1,26 @@
 { pkgs, ... }:
 
-let
-    nwg = pkgs.nwg-panel;
-in
 {
     systemd.user.services.nwg-panel = {
-        Unit.After = [ "graphical-session.target" ];
-        Service.ExecStart = "${nwg}/bin/nwg-panel";
-        Install.WantedBy = [ "graphical-session.target" ];
+      Unit = {
+        Description = "nwg-panel";
+        After = [ "graphical-session.target" ];
+      };
+
+      Service = {
+        ExecStart = "${pkgs.nwg-panel}/bin/nwg-panel";
+        Restart = "on-failure";
+      };
+
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
     };
 
     xdg.configFile."nwg-panel/config".source = ./nwg-panel/config;
 
-    home.packages = [ nwg ];
+    home.packages = with pkgs; [
+      nwg-panel
+      swaynotificationcenter
+    ];
 }
