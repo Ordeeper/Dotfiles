f7593f1a7d36759f31696870d75315ebb3f26d22
Ordeeper <pedrordeeper@gmail.com>
2026-04-09 11:19:33 -0300

    refactor: Enable Lsp tool

--- home/default/programs/opencode/config.nix
+++ home/default/programs/opencode/config.nix
@@ -10,12 +10,14 @@
       permission = {
         websearch = "allow";
         webfetch = "allow";
+        lsp = "allow";
       };
     };
   };
 
   home.sessionVariables = {
     OPENCODE_ENABLE_EXA = "1";
+    OPENCODE_EXPERIMENTAL_LSP_TOOL = "true";
   };
 
 }
