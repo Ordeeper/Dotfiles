3d311730ba1fa02afb209441a9abea4fe3eaf3ca
Ordeeper <pedrordeeper@gmail.com>
2026-04-29 16:10:06 -0300

    refactor: Improve fish config

--- home/default/programs/fish/config.nix
+++ home/default/programs/fish/config.nix
@@ -3,25 +3,25 @@
     enable = true;
 
     interactiveShellInit = ''
-      # Hide greeting
-      set -g fish_greeting
-
-      # Vi mode + cursor shapes
-      fish_vi_key_bindings
-      set fish_cursor_default block
-      set fish_cursor_insert line
-      set fish_cursor_replace_one underscore
-      set fish_cursor_visual block
-      set -g fish_vi_force_cursor 1
-
       if status is-interactive
-          and not set -q TMUX
-          and command -v tmux >/dev/null
-          and test -t 1
+          # Hide greeting
+          set -g fish_greeting
+
+          # Vi mode + cursor shapes
+          fish_vi_key_bindings
+          set fish_cursor_default block
+          set fish_cursor_insert line
+          set fish_cursor_replace_one underscore
+          set fish_cursor_visual block
+          set -g fish_vi_force_cursor 1
+
+          if not set -q TMUX; and command -v tmux >/dev/null; and test -t 1
               exec tmux
+          end
       end
     '';
   };
+
   imports = [
     ./keybinds.nix
   ];
--- home/default/programs/fish/keybinds.nix
+++ home/default/programs/fish/keybinds.nix
@@ -1,10 +1,12 @@
 {
   programs.fish.interactiveShellInit = ''
-    bind yy fish_clipboard_copy
-    bind p fish_clipboard_paste
-    bind -M insert \cF accept-autosuggestion
-    bind -M insert \cC self-insert expand-abbr
-    bind -M insert ' ' self-insert
+    function fish_user_key_bindings
+        fish_vi_key_bindings
+        bind yy fish_clipboard_copy
+        bind p fish_clipboard_paste
+        bind -M insert \cF accept-autosuggestion
+        bind -M insert \cC self-insert expand-abbr
+        bind -M insert ' ' self-insert
+    end
   '';
 }
-
