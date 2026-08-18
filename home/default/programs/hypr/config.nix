4e3bd1b06482ae0f049de49b9b937c68bd9c5c17
Ordeeper <pedrordeeper@gmail.com>
2026-08-18 13:20:11 -0300

    style(hy3): Use the DMS warning color for locked tabs

--- home/default/programs/hypr/config.nix
+++ home/default/programs/hypr/config.nix
@@ -81,7 +81,7 @@ in
               active_border = rgba colors.base0D "ff";
               active_text = rgba colors.base05 "ff";
               active_alt_monitor = rgba colors.base01 "ff";
-              active_alt_monitor_border = rgba colors.base0D "66";
+              active_alt_monitor_border = rgba colors.base0D "55";
               active_alt_monitor_text = rgba colors.base04 "ff";
               inactive = rgba colors.base01 "ff";
               inactive_border = rgba colors.base03 "01";
@@ -92,8 +92,8 @@ in
               urgent = rgba colors.base08 "ff";
               urgent_border = rgba colors.base08 "ff";
               urgent_text = rgba colors.base01 "ff";
-              locked = rgba colors.base0C "cc";
-              locked_border = rgba colors.base0C "ee";
+              locked = rgba colors.base09 "cc";
+              locked_border = rgba colors.base09 "ee";
               locked_text = rgba colors.base01 "ff";
             };
           };
