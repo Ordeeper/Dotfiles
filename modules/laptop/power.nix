9d7e210090e89ed96caa79790e1a94cb53c0f80a
Ordeeper <pedrordeeper@gmail.com>
2026-08-21 16:38:48 -0300

    feat(power): Map platform profiles and turbo per TLP tier

--- modules/laptop/power.nix
+++ modules/laptop/power.nix
@@ -26,8 +26,11 @@
     settings = {
       TLP_AUTO_SWITCH = 0;
       TLP_DEFAULT_MODE = "PRF";
-      PLATFORM_PROFILE_ON_AC = "balanced";
+      PLATFORM_PROFILE_ON_AC = "cool";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
       PLATFORM_PROFILE_ON_SAV = "quiet";
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 1;
       CPU_BOOST_ON_SAV = 0;
       START_CHARGE_THRESH_BAT0 = 75;
       STOP_CHARGE_THRESH_BAT0 = 80;
